67ebb16043127cff47d52e5fe9024872efc1436c
Aurélie <naoorelie@gmail.com>
2025-08-27 11:56:04 +0200

    avancé page d'accueil

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,47 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./pages/ErrorPage.tsx";
+import HomePage from "./pages/HomePage.tsx";
+import AproposPage from "./pages/AproposPage.tsx";
+import ContactPage from "./pages/ContactPage.tsx";
+import PrestationPage from "./pages/PrestationPage.tsx";
+import AdminPage from "./pages/AdminPage.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+    {
+        path:"/",
+        element:<App/>,
+        errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "",
+                element: <HomePage />
+            },
+            {
+                path:"/a-propos",
+                element: <AproposPage/>
+            },
+            {
+                path:"/contact",
+                element: <ContactPage/>
+            },
+            {
+                path:"/prestations",
+                element: <PrestationPage/>
+            },
+            {
+                path:"/admin",
+                element: <AdminPage/>
+            }
+        ]
+    }
+])
+
+const rootReactContainer = createRoot(
+    document.querySelector("#root") as HTMLElement,
+);
+
+rootReactContainer.render(
+    <RouterProvider router={router} />
+);
---
+++ frontend/src/pages/HomePage.tsx
@@ -0,0 +1,19 @@
+export default function HomePage() {
+    return (
+        <>
+            <h1 className="text-3xl text-center">Bienvenu</h1>
+            <div className="flex justify-center gap-8">
+                {/*{ albums.map((album, index) => (
+                <div>
+                    <Album album={album} key={index} />
+                <div>
+                ))}*/}
+                <div >01</div>
+                <div >02</div>
+                <div >03</div>
+                <div >04</div>
+            </div>
+        </>
+)
+    ;
+}
