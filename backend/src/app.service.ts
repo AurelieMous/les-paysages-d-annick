9acb331fd70ecf4b0d61600eefea6bedf4a31a79
Aurélie <naoorelie@gmail.com>
2025-12-03 12:27:18 +0100

    correction docker file docker compose

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-    getHello(): string {
-        return 'Photographer API is running! 📸';
-    }
+  getHello(): string {
+    return 'Hello World!';
+  }
 }
